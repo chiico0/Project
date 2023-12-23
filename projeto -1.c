a8791bb949d7d2cc7414bd97b8eeee26d725a605
chiico0 <153499363+chiico0@users.noreply.github.com>
2023-12-23 03:28:06 +0000

    Add files via upload

---
+++ projeto -1.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <string.h>
+
+#define MAXCOMPUT 100 // define o máximo de computadores
+#define MAXINTERV 500 // define p máximo de intervenções
+
+
+// Criação de uma Estrutura para representar uma intervenção 
+
+typedef struct {
+    char descricao[100];
+    char data[20];
+    } Intervenção
+
+
+// Criação de uma Estrura para representar um Portátil
+
+typedef struct {
+    char numero_serie[20]
+    Intervencao intervencoes[MAXINTERV]
+    int num_intervencoes;
+} ComputadorPortatil;
+
+
+//Função main
+
+int main(void) {
+    Computador computadores[MAXCOMPUT];
+
+// Atribuição de um número de serie único
+     int sprinf(computadores[i].numero_serie, "SN%D", i +1);
+    computadores[i].num_intervencoes = 0;
+}
+
+
+
